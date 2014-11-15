804e9b9efc35011e01811794e436ec5465253cb9
Krzysztof Kołek <krzysiek.koek6@gmail.com>
2014-11-15 13:08:00 +0100

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+puts 'CREATED ADMIN USER: ' << user.ema
+
+cities = %w[Warsaw Rzeszow Poznan Krakow Wroclaw Gdansk]
+
+company_names = %w[Lidl Biedronka Tesco]
+company_desc = ["Lidl jest tani", "Codziennie niskie ceny", "Duzo, tanio Tesco"]
+
+products_names = ["Bread", "Cammembert", "Apple juice", "Mineral water", "Beer"]
+products_desc = ["White bread", "Cammembert cheese", "Fresh apple juice", "Low-mineral water", "Kasztelan beer"]
+
+6.times do |n|
+  name = cities[n]
+  City.create!(name: name)
+end
+
+3.times do |n|
+  name = company_names[n]
+  desc = company_desc[n]
+  Company_Name.create!(name: name, description: desc)
+end

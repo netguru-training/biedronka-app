466bb1f3bc16b8b5dd4a2bb64b4941e551695f6c
Tomasz Jaśkiewicz <jaskiewicztomasz@gmail.com>
2014-11-15 16:13:50 +0100

    Added some corrections in models

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ActiveRecord::Base
   acts_as_taggable_on :categories
   belongs_to :shop
   has_many :promotions
+  has_many :shop_products
   has_many :shops, through: :shop_products
 end

97b31b5bf6e1c5fd0d8a39d7a54e47561f64bee2
Tomasz Jaśkiewicz <jaskiewicztomasz@gmail.com>
2014-11-15 12:30:33 +0100

    Added relation to Promotion model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115111452) do
+ActiveRecord::Schema.define(version: 20141115112659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20141115111452) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "product_id"
   end
 
   create_table "shop_products", force: true do |t|

5857196f7317a060a6dd2fc171780251ffc0ff3e
Krzysztof Kołek <krzysiek.koek6@gmail.com>
2014-11-15 12:44:19 +0100

    Add category column to product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115111452) do
+ActiveRecord::Schema.define(version: 20141115114150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20141115111452) do
     t.integer  "shop_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "category"
   end
 
   add_index "products", ["shop_id"], name: "index_products_on_shop_id", using: :btree

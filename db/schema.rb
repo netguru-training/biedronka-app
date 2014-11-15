7eb392c39bafa9e541b4a08229a241cb62dbf782
Tomasz Jaśkiewicz <jaskiewicztomasz@gmail.com>
2014-11-15 12:16:00 +0100

    Added CompanyName model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115105651) do
+ActiveRecord::Schema.define(version: 20141115111452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,16 @@ ActiveRecord::Schema.define(version: 20141115105651) do
     t.datetime "updated_at"
   end
 
+  create_table "company_names", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "products", force: true do |t|
     t.string   "name"
+    t.decimal  "base_price"
     t.string   "description"
     t.integer  "shop_id"
     t.datetime "created_at"

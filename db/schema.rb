1d84c2c42dfcb5dba7e958a3b1fd44609fd69e70
Tomasz Jaśkiewicz <jaskiewicztomasz@gmail.com>
2014-11-15 12:49:14 +0100

    Added column modifier to Promotion model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115112659) do
+ActiveRecord::Schema.define(version: 20141115114242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20141115112659) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "product_id"
+    t.decimal  "modifier",    precision: 8, scale: 2
   end
 
   create_table "shop_products", force: true do |t|

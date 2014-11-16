d346a22148b92816d4d153dcc09dfbf10c932be8
Krzysztof Kołek <krzysiek.koek6@gmail.com>
2014-11-16 11:40:02 +0100

    Add form for create and update product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,8 +8,32 @@ class ProductsController < ApplicationController
   end
 
   def create
+    self.product = Product.new(product_params)
+    if product.save
+      redirect_to products_path, notice: 'Product was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if product.update(product_params)
+      redirect_to products_path, notice: 'Product was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def show
   end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :description, :base_price, :shop_id, :category, :image)
+    end
+
 end

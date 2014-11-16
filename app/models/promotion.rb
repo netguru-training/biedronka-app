e1025c0a50975df726e569f58274c16fb295b35b
Krzysztof Kołek <krzysiek.koek6@gmail.com>
2014-11-16 15:14:09 +0100

    Change scope method

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,5 +1,5 @@
 class Promotion < ActiveRecord::Base
-  scope :actual_promotion, -> { where("start_date <= ? AND end_date >= ?", Time.now, Time.now)}
+  scope :actual_promotion, -> { where("start_date <= :my_date AND end_date >= :my_date", my_date: Time.zone.now)}
 
   belongs_to :product
 end

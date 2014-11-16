c78e555bead127bd528aec5e145321e70a613131
Tomasz Jaśkiewicz <jaskiewicztomasz@gmail.com>
2014-11-16 11:37:25 +0100

    Added destroy action to city controller and dependencies to city model

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,3 +1,9 @@
 class CitiesController < ApplicationController
   expose :cities
+  expose :city
+
+  def destroy
+    city.destroy
+    redirect_to root_path, notice: 'City successfully deleted'
+  end
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ActiveRecord::Base
-  has_many :shops
-  has_many :products, through: :shops
-  has_many :promotions, through: :products
+  has_many :shops, dependent: :destroy
+  has_many :products, through: :shops, dependent: :destroy
+  has_many :promotions, through: :products, dependent: :destroy
 end

b334eefcadadd4dc2f0e1e15f82ec720f90efa56
Tomasz Jaśkiewicz <jaskiewicztomasz@gmail.com>
2014-11-16 11:50:12 +0100

    Added edit and delete actions for cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,9 +1,26 @@
 class CitiesController < ApplicationController
+  before_action :is_signed_in, only: [:create, :update, :destroy]
+  before_action :verify_admin, only: [:create, :update, :destroy]
+
   expose :cities
   expose :city
+
+  def update
+    if city.update(city_params)
+      redirect_to cities_path, notice: 'City successfully updated'
+    else
+      render action: 'edit'
+    end
+  end
 
   def destroy
     city.destroy
     redirect_to root_path, notice: 'City successfully deleted'
+  end
+
+  private
+
+  def city_params
+    params.require(:city).permit(:name)
   end
 end
